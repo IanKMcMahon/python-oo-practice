8aa73cceded1c05fd3d281df576fbd90f34d897e
Ian McMahon <iankmcmahon@gmail.com>
2023-10-01 11:55:57 -0700

    WordFinder WIP

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,15 @@
 """Word Finder: finds random words from a dictionary."""
-file = open("words.txt", "r")
+wf = WordFinder("/Users/student/words.txt")
+import random 
+words = list()
 
-class WordFinder:
-    ...
+
+class WordFinder():
+    
+    def __init__(self, path):
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} words read")
+    def parse(self, dict_file):
+        return [w.strip() for w in dict_file]
+    def random(words):
