fbdd0d5277fa2ccc6279307ab78e3c2a3ef0c4ef
Ian McMahon <iankmcmahon@gmail.com>
2023-10-01 12:02:52 -0700

    WordFinder complete

--- wordfinder.py
+++ wordfinder.py
@@ -1,15 +1,17 @@
 """Word Finder: finds random words from a dictionary."""
-wf = WordFinder("/Users/student/words.txt")
 import random 
-words = list()
+path = "/Users/student/words.txt"
 
 
-class WordFinder():
-    
+class WordFinder(): 
+       
     def __init__(self, path):
         dict_file = open(path)
         self.words = self.parse(dict_file)
         print(f"{len(self.words)} words read")
+
     def parse(self, dict_file):
         return [w.strip() for w in dict_file]
-    def random(words):
+
+    def random(self):
+        return random.choice(self.words)
